5d466265ae978e5680e16c8e7a0a733af1df816e
Aditya Prayoga <aditya@kobol.io>
2018-08-20 17:30:09 +0800

    Improve installation script
    - Check for root/superuser priviledge
    - Install dependency packages

--- install.sh
+++ install.sh
@@ -2,11 +2,20 @@
 
 LOCAL_INSTALL=1
 INSTALL_PATH=""
+DEPS="fio stress-ng"
 
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root/superuser!"
+    exit 1
+fi
 
-if [[ $LOCAL_INSTALL -eq 1 ]]; then
+if [ $LOCAL_INSTALL -eq 1 ]; then
     INSTALL_PATH="/usr/local/"
 fi
+
+echo "Installing Dependencies"
+apt-get update
+apt-get install -y $DEPS
 
 cp -frv lib  "$INSTALL_PATH"
 cp -frv sbin "$INSTALL_PATH"
